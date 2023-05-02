133701bdd0fa93b2e4d497b0c8d455d1b0dc1120
binoll <onionlsfa@gmail.com>
2023-05-03 01:34:27 +0300

    Added new methods

--- main.cpp
+++ main.cpp
@@ -2,18 +2,8 @@
 #include "data_structures/set.h"
 
 int main() {
+    Set<int> set;
     BinaryTree<int> tree;
-
-
-    tree.add(7);
-
-    tree.add(9);
-
-    tree.add(9);
-
-    tree.add(5);
-
-    tree.remove(7);
 
     return 0;
 }
